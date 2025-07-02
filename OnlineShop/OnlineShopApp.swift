5967bffe0024e68bb3c5be407ea696c3b93482cd
AnastasiaNim <nastya.nimchenko@yandex.ru>
2025-07-02 15:02:25 +0300

    Initial Commit

---
+++ OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Anastasia N.  on 02.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
