2fd6e04c92828004876adc08b939bb3346ca74e3
AnastasiaNim <nastya.nimchenko@yandex.ru>
2025-07-24 17:28:21 +0300

    add card view

---
+++ OnlineShop/Views/CardScreen/View/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CartView.swift
+//  OnlineShop
+//
+//  Created by Anastasia N.  on 10.07.2025.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .navigationTitle("Cart")
+    }
+}
+
+#Preview {
+    CardView()
+}
