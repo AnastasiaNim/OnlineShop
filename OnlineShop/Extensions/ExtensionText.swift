dfa0d5b8df36e0ad77d1d72d9ccdec9aa1de310d
AnastasiaNim <nastya.nimchenko@yandex.ru>
2025-07-24 17:29:07 +0300

    add extension text

---
+++ OnlineShop/Extensions/ExtensionText.swift
@@ -0,0 +1,38 @@
+//
+//  ExtensionText.swift
+//  OnlineShop
+//
+//  Created by Anastasia N.  on 23.07.2025.
+//
+
+import SwiftUI
+
+extension Text {
+    func titleFont() -> some View {
+        self
+            .foregroundStyle(.primary)
+            .font(.system(size: 18, weight: .bold, design: .rounded))
+            .opacity(0.8)
+    }
+    func subtitle() -> some View {
+        self
+            .foregroundStyle(.primary)
+            .font(.system(size: 15, weight: .regular, design: .rounded))
+            .opacity(0.8)
+    }
+}
+
+struct FontView: View {
+    var body: some View {
+        Text("Title")
+            .titleFont()
+        
+        Text("Subtitle")
+            .subtitle()
+    }
+}
+
+
+#Preview {
+    FontView()
+}
