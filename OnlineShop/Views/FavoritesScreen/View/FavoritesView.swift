e19332b4a29be1b54812f8d082eb41b1a8a82bf6
AnastasiaNim <nastya.nimchenko@yandex.ru>
2025-07-25 13:19:44 +0300

    add favorites view

--- OnlineShop/Views/FavoritesScreen/View/FavoritesView.swift
+++ OnlineShop/Views/FavoritesScreen/View/FavoritesView.swift
@@ -14,13 +14,16 @@ struct FavoritesView: View {
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             LazyVGrid(columns: columns) {
-                ForEach(favoritesItems) {item in
-                    NavigationLink(destination: EmptyView()) {
+                ForEach(favoritesItems) { item in
+                    NavigationLink(destination: DetailView(product: item)) {
                         ProductCardView(product: item)
                     }
+                    .buttonStyle(.plain)
                 }
             }
         }
+        .padding(.horizontal, 10)
+        .background(.secondary.opacity(0.3))
         .navigationTitle("Favorites")
     }
 }
