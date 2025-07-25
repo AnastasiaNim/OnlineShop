c15b707198d3070f9aacd7ba34ad3dd18f20cb87
AnastasiaNim <nastya.nimchenko@yandex.ru>
2025-07-25 13:23:38 +0300

    update main view

--- OnlineShop/Views/MainScreen/View/MainView.swift
+++ OnlineShop/Views/MainScreen/View/MainView.swift
@@ -9,52 +9,41 @@ import SwiftUI
 import FirebaseFirestore
 
 struct MainView: View {
+    @EnvironmentObject var vm: ViewModel
     @FirestoreQuery(collectionPath: "shop") var items: [Product]
     private var columns: [GridItem] = Array(repeating: GridItem(), count: 2)
     var body: some View {
-        ZStack {
-            LinearGradient(
-                gradient: Gradient(colors: [Color.pink.opacity(0.6),Color.white, Color.pink.opacity(0.6)]),
-                startPoint: .topLeading,
-                endPoint: .bottomTrailing
-            )
-            .ignoresSafeArea()
-            
-            Color.white
-                .opacity(0.3)
-                .blur(radius: 50)
-                .blendMode(.plusLighter)
-                .ignoresSafeArea()
-            
-            NavigationStack {
-                ScrollView(.vertical, showsIndicators: false) {
-                    LazyVGrid(columns: columns) {
-                        ForEach(items) { item in
+        NavigationStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVGrid(columns: columns) {
+                    ForEach(items) { item in
+                        NavigationLink(destination: DetailView(product: item)) {
                             ProductCardView(product: item)
                         }
+                        .buttonStyle(.plain)
                     }
                 }
-                .padding(.horizontal, 10)
-                .background(Color.secondary.opacity(0.2))
-                .shadow(color: .secondary.opacity(0.2), radius: 8, x: 5, y: 8)
-                .navigationTitle("Products")
-                .toolbar {
-                    ToolbarItem( placement: .topBarLeading) {
-                        NavigationLink(destination: FavoritesView()) {
-                            Image(systemName: "heart.fill")
-                                .font(.title2)
-                            
-                        }
-                        .buttonStyle(.plain)
+            }
+            .padding(.horizontal, 10)
+            .background(Color.secondary.opacity(0.2))
+            .shadow(color: .secondary.opacity(0.2), radius: 8, x: 5, y: 8)
+            .navigationTitle("Products")
+            .toolbar {
+                ToolbarItem( placement: .topBarLeading) {
+                    NavigationLink(destination: FavoritesView()) {
+                        Image(systemName: "heart.fill")
+                            .font(.title2)
                         
                     }
-                    ToolbarItem( placement: .topBarTrailing) {
-                        NavigationLink(destination: CardView()) {
-                            Image(systemName: "cart.fill")
-                                .font(.title2)
-                        }
-                        .buttonStyle(.plain)
+                    .buttonStyle(.plain)
+                    
+                }
+                ToolbarItem( placement: .topBarTrailing) {
+                    NavigationLink(destination: CartView()) {
+                        Image(systemName: "cart.fill")
+                            .font(.title2)
                     }
+                    .buttonStyle(.plain)
                 }
             }
         }
@@ -63,4 +52,5 @@ struct MainView: View {
 
 #Preview {
     MainView()
+        .environmentObject(ViewModel())
 }
