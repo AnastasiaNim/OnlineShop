26ad9b6475a0ffb246e37518a93406cdd84bb5d8
AnastasiaNim <nastya.nimchenko@yandex.ru>
2025-07-25 13:15:41 +0300

    add view model

---
+++ OnlineShop/ViewModel/ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModel.swift
+//  OnlineShop
+//
+//  Created by Anastasia N.  on 23.07.2025.
+//
+
+import Foundation
+import FirebaseFirestore
+
+final class ViewModel: ObservableObject {
+    private let db = Firestore.firestore().collection("shop")
+    
+    func toggleFavorites(product: Product) {
+        guard let id = product.id else { return }
+        db.document(id).updateData(["isFavorite": !product.isFavorite])
+    }
+}
